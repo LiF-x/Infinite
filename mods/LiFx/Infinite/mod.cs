63872b4b67b2a91fef56144718a51370d99c5779
WARPED IBUN <84038557+matty5350@users.noreply.github.com>
2023-11-12 22:24:39 +0000

    mod addition

---
+++ mods/LiFx/Infinite/mod.cs
@@ -0,0 +1,36 @@
+/**
+* <author>Christophe Roblin</author>
+* <url>lifxmod.com</url>
+* <credits></credits>
+* <description>Changes player.cs for server and client, requires basilmod for full effect</description>
+* <license>GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007</license>
+*/
+
+
+if (!isObject(LiFxInfinite))
+{
+    new ScriptObject(LiFxInfinite)
+    {
+    };
+}
+
+
+package LiFxInfinite
+{
+  function LiFxInfinite::setup() {
+    LiFx::registerCallback($LiFx::hooks::onInitServerDBChangesCallbacks, changePlayerData, LiFxInfinite);
+    LiFx::registerCallback($LiFx::hooks::onPostInitCallbacks, RegisterBasil, LiFxInfinite);
+  }
+  function LiFxInfinite::version() {
+    return "0.0.1";
+  }
+  function LiFxInfinite::RegisterBasil() {
+    BasilMod::pack_content("art/datablocks/player.cs", "mods/LiFx/Infinite/player.cs");
+  }
+  function LiFxInfinite::changePlayerData() {
+    PlayerData.sprintCooldown = 0;
+    exec("./player.cs");
+  }
+};
+activatePackage(LiFxInfinite);
+LiFx::registerCallback($LiFx::hooks::mods, setup, LiFxInfinite);
